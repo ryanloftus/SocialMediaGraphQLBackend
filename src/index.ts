c166d331d7a2cdd19e6a1845f4d7a8f44fcab96d
ryanloftus <ryanloftus02@outlook.com>
2022-12-29 20:31:41 -0500

    adding redis

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,56 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import AppDataSource from './data-source.js'
-import { buildSchema } from 'type-graphql';
+import { ApolloServer } from '@apollo/server'
+import { expressMiddleware } from '@apollo/server/express4'
+import Redis from 'ioredis'
+import session from 'express-session'
+import connectRedis from 'connect-redis'
+import express from 'express'
+import cors from 'cors'
+import { json } from 'body-parser'
+import { buildSchema } from 'type-graphql'
 import UserResolver from './resolvers/user-resolver.js'
+import AppDataSource from './data-source.js'
 
 try {
+    // connect to Postgres
+    await AppDataSource.initialize()
+
+    const app = express()
+
+    const RedisStore = connectRedis(session)
+
+    app.use(
+        session({
+            name: 'sid',
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
+                httpOnly: true,
+                sameSite: 'lax',
+            },
+            store: new RedisStore({
+                client: new Redis(),
+                disableTouch: true,
+            }),
+            saveUninitialized: false,
+            secret: 'keyboard-cat', // TODO: make a random string, add to environment variables
+            resave: false,
+        })
+    )
+
     const server = new ApolloServer({
         schema: await buildSchema({
             resolvers: [UserResolver],
             validate: false,
-        })
-    });
-
-    await AppDataSource.initialize()
-
-    const { url } = await startStandaloneServer(server, {
-        listen: { port: 4000 },
+        }),
     })
 
-    console.log(`🚀 Server ready at: ${url}`)
+    app.use(
+        '/graphql',
+        cors<cors.CorsRequest>(),
+        json(),
+        expressMiddleware(server),
+    )
+
+    app.listen(4000, () => console.log(`🚀 Server ready at: http://localhost:4000`))
 } catch (error) {
     console.error("Error: ", error)
 }
