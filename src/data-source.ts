43b72e934fc27a4c10ffdc5b94292155d76ab03c
ryanloftus <ryanloftus02@outlook.com>
2023-04-14 17:34:35 -0400

    using env vars for db

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,11 +3,11 @@ import { DataSource } from "typeorm"
 
 const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "ryanl",
+    host: process.env.HOST,
+    port: Number(process.env.DATABASE_PORT),
+    username: process.env.DATABASE_USER,
     password: null,
-    database: "social-media",
+    database: process.env.DATABASE_NAME,
     synchronize: true,
     logging: true,
     entities: ["src/entities/**/*.ts"],
